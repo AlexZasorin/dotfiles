75ed36858e9252fa9e0141a88a8a0f875bd1a165
Alex Zasorin <zasorin.alek@gmail.com>
2024-06-11 23:48:41 -0700

    add copy script for nix

---
+++ .chezmoiscripts/run_once_after_install-setup-nix.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Ensure the target directory exists
+sudo mkdir -p /etc/nixos
+
+# Copy configuration files to /etc/nixos
+sudo cp $HOME/.local/share/chezmoi/nixos/configuration.nix /etc/nixos/configuration.nix
+sudo cp $HOME/.local/share/chezmoi/nixos/flake.nix /etc/nixos/flake.nix
+sudo cp $HOME/.local/share/chezmoi/nixos/flake.lock /etc/nixos/flake.lock
+
+echo "testing lol"
