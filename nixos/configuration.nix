39b0835e30e84ffda0a33b58bd14c91ec2494e15
Alex Zasorin <zasorin.alek@gmail.com>
2024-06-11 22:58:26 -0700

    add nixos files to chezmoi repo

---
+++ nixos/configuration.nix
@@ -0,0 +1,281 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader
+  boot = {
+    supportedFilesystems = [ "ntfs" ];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = true;
+        efiSupport = true;
+	gfxmodeEfi = "2560x1440,1920x1080";
+	default = "saved";
+	theme = pkgs.stdenv.mkDerivation {
+          pname = "grub2-solarized-dark";
+	  version = "daabc2c6d6179bd99f20bfc46d25f6433eecdd68";
+	  src = pkgs.fetchFromGitHub {
+	    owner = "bino-faata";
+	    repo = "grub2-solarized-dark";
+	    rev = "daabc2c6d6179bd99f20bfc46d25f6433eecdd68";
+	    sha256 = "1awbi6z8016l7vmlkwvhfaw0b5a5chjjqb26vl0swarfpb0d73ky";
+	  };
+	  installPhase = "cp -r . $out";
+	};
+      };
+    };
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "deimos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.firewall = { 
+    enable = true;
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+  };  
+
+  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+
+  services.resolved = {
+    enable = true;
+    domains = [ "~." ];
+    fallbackDns = [ "8.8.8.8" "8.8.4.4" ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+  time.hardwareClockInLocalTime = true; 
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+  
+  systemd.services.kanata = {
+    enable = true;
+    description = "run kanata";
+    unitConfig = {
+      type = "simple";
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.kanata}/bin/kanata -c /home/solyx/.config/kanata/kanata.kbd";
+      Restart = "always";
+      RestartSec = 10;
+      User = "root";
+      Group = "root";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+  
+  services.dnscache.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.solyx = {
+    isNormalUser = true;
+    description = "solyx";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      chezmoi
+      zsh
+      gitui
+      htop
+      gh
+      helix
+      kubectl
+      kubectx
+      zoxide
+      zellij
+      discord	
+      vesktop
+      spotify
+      jetbrains-toolbox
+      vscode
+      git
+      eza
+      tmux
+      bottom
+      lazydocker
+      lazygit
+      asciiquarium
+      broot
+      kubectl
+      kubectx
+      fzf
+      kanata
+      jsonnet
+      filelight
+      heroic
+      libsForQt5.bismuth
+      libsForQt5.kdeconnect-kde
+      docker
+      todoist-electron
+      atuin
+      gsmartcontrol
+      btop
+      papirus-icon-theme
+      dig
+      traceroute
+    ];
+  };
+
+  programs = {
+    zsh = {
+      enable = true;
+    };
+    kdeconnect = {
+      enable = true;
+    };
+    nix-ld = {
+      enable = true;
+    };
+    ssh = {
+      startAgent = true;
+      extraConfig = "AddressFamily inet";
+    };
+    noisetorch = {
+      enable = true;
+    };
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
+    wezterm
+    gcc
+    clang
+    zig
+    wget
+    curl
+    os-prober
+    which
+    inxi
+    keychain
+    xclip
+    alejandra
+    lua-language-server
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Meslo" ]; })
+    jetbrains-mono
+    noto-fonts-cjk-sans
+  ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?  
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath; 
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+
+}
