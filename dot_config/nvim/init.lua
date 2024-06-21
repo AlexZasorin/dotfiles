3bf217b18cdd53bc64fc9afa5956bcb5490f5c26
Alex Zasorin <zasorin.alek@gmail.com>
2024-06-20 21:35:51 -0700

    update neovim or whatever

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -375,6 +375,7 @@ require('neodev').setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
