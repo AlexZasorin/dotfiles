da76e51b0b75eb5edf10af8d322d71f3c74f9a22
Aleksey Zasorin <zasorin.alek@gmail.com>
2024-06-24 10:30:34 -0700

    enable relative numbers

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -102,11 +102,12 @@ vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 vim.opt.shiftwidth = 2 -- 2 spaces for indent width
 vim.opt.expandtab = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
