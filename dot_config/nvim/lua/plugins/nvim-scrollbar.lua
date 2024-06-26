92ad25d78b63d576b8b6b6c28fe7d440992572ba
Alex Zasorin <zasorin.alek@gmail.com>
2024-06-26 14:46:15 -0700

    add nvim-scrollbar plugin

---
+++ dot_config/nvim/lua/plugins/nvim-scrollbar.lua
@@ -0,0 +1,12 @@
+return {
+  'petertriho/nvim-scrollbar',
+  config = function()
+    local colorbuddy = require 'colorbuddy'
+    local colors = require('neosolarized').setup({}).colors
+    require('scrollbar').setup {
+      handle = {
+        color = colorbuddy.Color.to_vim(colors.base1),
+      },
+    }
+  end,
+}
