79aba2a26dabf739bdbba02af08d75e0ca0cbe0f
Aleksey Zasorin <zasorin.alek@gmail.com>
2024-06-30 00:51:58 -0700

    51 current  2024-06-30 00:51:53  24.11.20240627.b2852eb   6.6.36                          *

--- dot_config/nixos/laptop.nix
+++ dot_config/nixos/laptop.nix
@@ -33,15 +33,15 @@
   systemd.sleep.extraConfig = "HibernateDelaySec=30s";
   networking.networkmanager.wifi.powersave = false;
 
-  systemd.services.ath11k-suspend = {
-    description = "Suspend: rmmod ath11k_pci";
-    before = ["sleep.target" "suspend-then-hibernate.target"];
-    wantedBy = ["sleep.target" "suspend-then-hibernate.target"];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.kmod}/bin/rmmod ath11k_pci";
-    };
-  };
+  # systemd.services.ath11k-suspend = {
+  #   description = "Suspend: rmmod ath11k_pci";
+  #   before = ["sleep.target" "suspend-then-hibernate.target"];
+  #   wantedBy = ["sleep.target" "suspend-then-hibernate.target"];
+  #   serviceConfig = {
+  #     Type = "simple";
+  #     ExecStart = "${pkgs.kmod}/bin/rmmod ath11k_pci";
+  #   };
+  # };
 
   systemd.services.ath11k-resume = {
     description = "Resume: modprobe ath11k_pci";
