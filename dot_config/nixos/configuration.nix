87736ba9745e83c3afe09476ff979c48a1f15523
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-05 17:21:30 -0700

    245 current  2024-07-05 17:21:26  24.11.20240703.9f4128e  6.6.36                          *

--- dot_config/nixos/configuration.nix
+++ dot_config/nixos/configuration.nix
@@ -201,7 +201,7 @@
     serviceConfig = {
       Type = "oneshot";
       ExecStart = [
-        "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh"
+        "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh && export SSH_AUTH_SOCK"
         # "export SSH_AUTH_SOCK"
       ];
       Environment = "DISPLAY=:0";
