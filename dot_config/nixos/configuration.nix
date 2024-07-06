fd4669907abd01a6f2fe9a73b59db7d7445d69e2
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-05 17:15:32 -0700

    242 current  2024-07-05 17:15:28  24.11.20240703.9f4128e  6.6.36                          *

--- dot_config/nixos/configuration.nix
+++ dot_config/nixos/configuration.nix
@@ -204,6 +204,7 @@
         "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh"
         "export SSH_AUTH_SOCK"
       ];
+      Environment = "DISPLAY=:0";
     };
   };
 
