ed022d5cd58e2cfa3f4ea2094bb69557412e328a
Aleksey Zasorin <zasorin.alek@gmail.com>
2024-07-08 00:13:06 -0700

    79 current  2024-07-08 00:08:42  24.11.20240703.9f4128e   6.6.36                          *

--- dot_config/nixos/configuration.nix
+++ dot_config/nixos/configuration.nix
@@ -324,6 +324,7 @@
     os-prober
     pciutils
     traceroute
+    usbutils
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     vlc
     wezterm
