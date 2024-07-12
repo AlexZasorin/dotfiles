917db8d7119df195b9aa706cc15016dd6781c0b6
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-12 14:22:36 -0700

    zellij new tab improvements

--- scripts/executable_zellij-new-tab.sh
+++ scripts/executable_zellij-new-tab.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-read -p "Enter a value: " input_value
+# set pane name
+zellij action rename-pane "New Tab"
 
-zoxide_result=$(zoxide query "$input_value")
+# prompt for a tab/dir name
+print -nP "%B%F{blue}Name%f%b: %F{yellow}"
+read "tab_name"
+print -n "%f"
+
+zoxide_result=$(zoxide query "$tab_name")
 
 if [ $? -eq 0 ] && [ -n "$zoxide_result" ]; then
-    zellij action new-tab --layout default --name "$input_value" --cwd "$zoxide_result"
+    zellij action new-tab --layout default --name "$tab_name" --cwd "$zoxide_result"
 else
-    echo "Error: zoxide query failed or returned empty result."
-    exit 1
+    zellij action new-tab --layout default --name "$tab_name"
 fi
