36ec7d999f21dc02ea6757fb3fa5e6e8498758cb
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-16 22:03:13 -0700

    add yaml schemas to nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -605,6 +605,20 @@ require('lazy').setup({
             },
           },
         },
+        yamlls = {
+          settings = {
+            yaml = {
+              schemaStore = {
+                -- You must disable built-in schemaStore support if you want to use
+                -- this plugin and its advanced options like `ignore`.
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = '',
+              },
+              schemas = require('schemastore').yaml.schemas(),
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
