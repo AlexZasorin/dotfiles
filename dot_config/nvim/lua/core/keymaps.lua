efe45cbfd4f2b0c575beb1eaa56956a5eb37b4de
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-18 10:19:12 -0700

    add keymaps for repeating indenting and moving lines up and down

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -2,5 +2,10 @@ local keymap = vim.keymap
 
 local opts = { noremap = true, silent = true }
 
-keymap.set('n', '<', '<gv', opts)
-keymap.set('n', '>', '>gv', opts)
+keymap.set({ 'n', 'v' }, '<', '<gv', opts)
+keymap.set({ 'n', 'v' }, '>', '>gv', opts)
+
+keymap.set('n', '<A-j>', ':m .+1<CR>==') -- move line up
+keymap.set('n', '<A-k>', ':m .-2<CR>==') -- move line down
+keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv") -- move line up(v)
+keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv") -- move line down(v)
