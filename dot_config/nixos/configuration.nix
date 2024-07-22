c1420fcdeb0d83cff96998c8919585979ef00928
Aleksey Zasorin <zasorin.alek@gmail.com>
2024-07-21 22:15:04 -0700

    86 current  2024-07-21 22:14:47  24.11.20240719.1d9c2c9   6.6.41                          *

--- dot_config/nixos/configuration.nix
+++ dot_config/nixos/configuration.nix
@@ -146,7 +146,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.pam.services.sddm.enableGnomeKeyring = true;
