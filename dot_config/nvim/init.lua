80735cb1432848f02a9e605ef65cd388d000eb56
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-23 16:44:32 -0700

    enable and configure folding

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -159,6 +159,13 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldtext = ''
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldnestmax = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
