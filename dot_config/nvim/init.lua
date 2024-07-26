f5e2af7380576b8daaed44d05f0e7516986eb04c
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-26 01:23:02 -0700

    remove unnecessary comments

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -939,24 +939,6 @@ require('lazy').setup({
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require('plugins.debug'),
-  -- require('plugins.indent_line'),
-  -- require('plugins.lint'),
-  -- require('plugins.autopairs'),
-  -- require('plugins.neo-tree'),
-  -- require('plugins.gitsigns'), -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
   { import = 'plugins' },
 }, {
   ui = {
