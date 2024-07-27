0ed6e385c418c57f28e083a9ebb8777ac2a2af79
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-26 17:52:49 -0700

    enable chezmoi plugin watch on edit

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -3,7 +3,18 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     require('chezmoi').setup({
-      -- your configurations
+      edit = {
+        watch = true,
+        force = false,
+      },
+      notification = {
+        on_open = true,
+        on_apply = true,
+        on_watch = true,
+      },
+      telescope = {
+        select = { '<CR>' },
+      },
     })
   end,
 }
