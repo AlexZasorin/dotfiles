a03c81cc631a1dd7e528848744dabd7b48588c9f
Alex Zasorin <zasorin.alek@gmail.com>
2024-07-28 01:43:31 -0700

    add chezmoi template synax highlighting

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,20 +1,30 @@
 return {
-  'xvzc/chezmoi.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('chezmoi').setup({
-      edit = {
-        watch = true,
-        force = false,
-      },
-      notification = {
-        on_open = true,
-        on_apply = true,
-        on_watch = true,
-      },
-      telescope = {
-        select = { '<CR>' },
-      },
-    })
-  end,
+  {
+    -- highlighting for chezmoi files template files
+    'alker0/chezmoi.vim',
+    init = function()
+      vim.g['chezmoi#use_tmp_buffer'] = 1
+      vim.g['chezmoi#source_dir_path'] = os.getenv('HOME') .. '/.local/share/chezmoi'
+    end,
+  },
+  {
+    'xvzc/chezmoi.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('chezmoi').setup({
+        edit = {
+          watch = true,
+          force = false,
+        },
+        notification = {
+          on_open = true,
+          on_apply = true,
+          on_watch = true,
+        },
+        telescope = {
+          select = { '<CR>' },
+        },
+      })
+    end,
+  },
 }
