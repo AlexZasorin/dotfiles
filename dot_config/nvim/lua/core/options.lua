4a5206184cc536180b7ad7401828f27c6c321616
Alex Zasorin <zasorin.alek@gmail.com>
2024-08-01 21:50:11 -0700

    fix missing options

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -3,12 +3,13 @@ local opt = vim.opt
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode (in keymaps.lua)
-opt.hlsearch = true
+--  For more options, you can see `:help option-list`
 
 -- Tabs & Indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
@@ -16,20 +17,25 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
--- Set highlight on search
-opt.hlsearch = true
-
 -- Make line numbers default
-opt.relativenumber = true
 opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+opt.relativenumber = true
 
--- Enable mouse mode
+-- Enable mouse mode, can be useful for resizing splits for example!
 opt.mouse = 'a'
 
+-- Don't show the mode, since it's already in the status line
+opt.showmode = false
+
 -- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-opt.clipboard = 'unnamedplus'
+vim.schedule(function()
+  opt.clipboard = 'unnamedplus'
+end)
 
 -- Enable break indent
 opt.breakindent = true
@@ -37,22 +43,42 @@ opt.breakindent = true
 -- Save undo history
 opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or capital in search
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 opt.ignorecase = true
 opt.smartcase = true
-opt.incsearch = true
 
 -- Keep signcolumn on by default
 opt.signcolumn = 'yes'
 
 -- Decrease update time
 opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
 opt.timeoutlen = 300
 
--- Set completeopt to have a better completion experience
-opt.completeopt = 'menuone,noselect'
+-- Configure how new splits should be opened
+opt.splitright = true
+opt.splitbelow = true
 
--- NOTE: You should make sure your terminal supports this
-opt.termguicolors = true
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-opt.autowrite = true
+-- Preview substitutions live, as you type!
+opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+opt.foldmethod = 'expr'
+opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+opt.foldtext = ''
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.foldnestmax = 4
