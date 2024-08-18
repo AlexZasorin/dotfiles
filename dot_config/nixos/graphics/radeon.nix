bc5b3041d69f44c0e1151ed616c3ba44c25e8c72
Aleksey Zasorin <zasorin.alek@gmail.com>
2024-08-18 16:17:06 -0700

    88 current  2024-08-18 16:16:59  24.11.20240804.cb9a96f   6.6.44                          *

--- dot_config/nixos/graphics/radeon.nix
+++ dot_config/nixos/graphics/radeon.nix
@@ -11,15 +11,15 @@
     "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
   ];
 
-  hardware.opengl.extraPackages = with pkgs; [
+  hardware.graphics.extraPackages = with pkgs; [
     rocmPackages.clr.icd
     amdvlk
   ];
 
   # For 32 bit applications
-  hardware.opengl.extraPackages32 = with pkgs; [
+  hardware.graphics.extraPackages32 = with pkgs; [
     driversi686Linux.amdvlk
   ];
 
-  hardware.opengl.driSupport32Bit = true; # For 32 bit applications
+  hardware.graphics.enable32Bit = true; # For 32 bit applications
 }
