fc0b4b03c46b864536f14ebc24c60b240380cbff
Alex Zasorin <zasorin.alek@gmail.com>
2024-12-01 21:51:11 -0800

    nvim(conform): enable alejandra formatter for nix files

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -42,6 +42,7 @@ return {
       typescript = { 'prettierd', 'prettier', stop_after_first = true },
       typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      nix = { 'alejandra' },
       rust = { 'rustfmt', lsp_format = 'fallback' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
