10ee61a42ffc018f00b58e25c888b3f7d3f25812
Alex Zasorin <zasorin.alek@gmail.com>
2024-12-25 23:15:14 -0800

    nvim(lspconfig): try different pyright analysis settings

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -167,7 +167,12 @@ return {
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       basedpyright = {
-        disableOrganizeImports = true,
+        analysis = {
+          diagnosticMode = 'workpsace',
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = true,
+        },
+        disableOrganzeImports = true,
       },
       bashls = {},
       cssls = {},
