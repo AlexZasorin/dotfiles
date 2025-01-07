346d056aee87d1a69772cdc90179f7f74627c2c9
Alex Zasorin <zasorin.alek@gmail.com>
2025-01-06 21:49:51 -0800

    refactor(treesitter): to use Meta-s for incremental selection

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -67,7 +67,7 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<Leader>vv',
+          init_selection = '<M-s>',
           node_incremental = '<TAB>',
           scope_incremental = '<Leader>vs',
           node_decremental = '<S-TAB>',
