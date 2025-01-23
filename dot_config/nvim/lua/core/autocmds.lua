0f79de1b49e29ed4330bdde4d343947fe2ddb651
Alex Zasorin <zasorin.alek@gmail.com>
2025-01-23 14:21:04 -0800

    feat(nvim): allow quiting neotest-output with q

--- dot_config/nvim/lua/core/autocmds.lua
+++ dot_config/nvim/lua/core/autocmds.lua
@@ -73,6 +73,7 @@ vim.api.nvim_create_autocmd('FileType', {
     'qf',
     'startuptime',
     'gitsigns.blame',
+    'neotest-output',
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
