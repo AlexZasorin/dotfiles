d59047e2beadca537874fb27c6a03fa4e14a3b48
Alex Zasorin <zasorin.alek@gmail.com>
2025-03-24 20:42:54 +0000

    refactor(nvim): improve note taking commands

--- dot_config/nvim/lua/core/usercmds.lua
+++ dot_config/nvim/lua/core/usercmds.lua
@@ -1,9 +1,21 @@
-vim.api.nvim_create_user_command('NewDailyPersonal', function()
-  local filename = '~/Repos/notes/personal/daily/' .. os.date('%Y-%m-%d') .. '.md'
-  vim.cmd('e ' .. filename)
-end, {})
+vim.api.nvim_create_user_command('NewNotePersonal', function(opts)
+  local full_path = '~/Repos/notes/personal/misc/' .. opts.args .. '.md'
+  vim.cmd('e ' .. full_path)
+end, { nargs = 1, desc = 'Filename' })
 
-vim.api.nvim_create_user_command('NewDailyWork', function()
-  local filename = '~/Repos/notes/work/daily/' .. os.date('%Y-%m-%d') .. '.md'
-  vim.cmd('e ' .. filename)
-end, {})
+vim.api.nvim_create_user_command('NewNoteWork', function(opts)
+  local full_path = '~/Repos/notes/work/misc/' .. opts.args .. '.md'
+  vim.cmd('e ' .. full_path)
+end, { nargs = 1, desc = 'Filename' })
+
+vim.api.nvim_create_user_command('NewDailyPersonal', function(opts)
+  local filename = opts.args ~= '' and opts.args or os.date('%Y-%m-%d')
+  local full_path = '~/Repos/notes/personal/daily/' .. filename .. '.md'
+  vim.cmd('e ' .. full_path)
+end, { nargs = '?', desc = 'Filename' })
+
+vim.api.nvim_create_user_command('NewDailyWork', function(opts)
+  local filename = opts.args ~= '' and opts.args or os.date('%Y-%m-%d')
+  local full_path = '~/Repos/notes/work/daily/' .. filename .. '.md'
+  vim.cmd('e ' .. full_path)
+end, { nargs = '?', desc = 'Filename' })
