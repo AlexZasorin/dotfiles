ebbc8c80b083240eb36b5cbc963ed23af2023133
Alex Zasorin <zasorin.alek@gmail.com>
2025-03-29 14:45:10 -0700

    feat(neovim): enable confirmation dialogs

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -115,6 +115,11 @@ opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 opt.scrolloff = 10
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+opt.confirm = true
+
 opt.foldmethod = 'expr'
 opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 opt.foldtext = ''
