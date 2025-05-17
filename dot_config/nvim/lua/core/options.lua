8e275ce19d937375b6140d8b01248707496bc2e0
Alex Zasorin <zasorin.alek@gmail.com>
2025-05-17 13:07:18 -0700

    chore(nvim): add ts to markdown_fenced_languages

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -6,6 +6,10 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.g.markdown_fenced_languages = {
+  'ts=typescript',
+}
+
 o.termguicolors = true
 
 -- [[ Setting options ]]
