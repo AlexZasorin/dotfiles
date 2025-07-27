1a2a6b393f71c2483d7ebe68f51e4a6ac44b4c35
Alex Zasorin <zasorin.alek@gmail.com>
2025-07-27 11:50:56 -0700

    feat(nvim): enable single file support

--- dot_config/nvim/lua/plugins/typescript-tools.lua
+++ dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -32,7 +32,7 @@ return {
         },
       },
       root_dir = util.root_pattern('package.json', 'tsconfig.json'),
-      single_file_support = false,
+      single_file_support = true,
     })
     local keymap_group = vim.api.nvim_create_augroup('TSToolsKeymaps', { clear = true })
     local opts = { noremap = true, silent = true }
