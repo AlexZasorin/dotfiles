381c8e0057d3258a8ab09e2644183c623c216a15
Alex Zasorin <zasorin.alek@gmail.com>
2025-07-30 09:09:00 -0700

    refactor(nvim): rename note user cmds

--- dot_config/nvim/lua/core/usercmds.lua
+++ dot_config/nvim/lua/core/usercmds.lua
@@ -1,20 +1,20 @@
-vim.api.nvim_create_user_command('NewNotePersonal', function(opts)
+vim.api.nvim_create_user_command('NewPersonalNote', function(opts)
   local full_path = '~/Repos/notes/personal/misc/' .. opts.args .. '.md'
   vim.cmd('e ' .. full_path)
 end, { nargs = 1, desc = 'Filename' })
 
-vim.api.nvim_create_user_command('NewNoteWork', function(opts)
+vim.api.nvim_create_user_command('NewWorkNote', function(opts)
   local full_path = '~/Repos/notes/work/misc/' .. opts.args .. '.md'
   vim.cmd('e ' .. full_path)
 end, { nargs = 1, desc = 'Filename' })
 
-vim.api.nvim_create_user_command('NewDailyPersonal', function(opts)
+vim.api.nvim_create_user_command('NewPersonalToDo', function(opts)
   local filename = opts.args ~= '' and opts.args or os.date('%Y-%m-%d')
   local full_path = '~/Repos/notes/personal/daily/' .. filename .. '.md'
   vim.cmd('e ' .. full_path)
 end, { nargs = '?', desc = 'Filename' })
 
-vim.api.nvim_create_user_command('NewDailyWork', function(opts)
+vim.api.nvim_create_user_command('NewWorkToDo', function(opts)
   local filename = opts.args ~= '' and opts.args or os.date('%Y-%m-%d')
   local full_path = '~/Repos/notes/work/daily/' .. filename .. '.md'
   vim.cmd('e ' .. full_path)
