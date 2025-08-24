ae256d74f4cf636dd3f4b42291c949455dd4e0dd
Alex Zasorin <zasorin.alek@gmail.com>
2025-08-24 03:19:13 +0000

    feat(nixos): add user to docker group

--- dot_config/nixos/configurations/wsl.nix
+++ dot_config/nixos/configurations/wsl.nix
@@ -16,7 +16,7 @@
   wsl.defaultUser = "solyx";
   users.users.solyx = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = ["wheel", "docker"];
   };
 
   services.atuin.enable = true;
