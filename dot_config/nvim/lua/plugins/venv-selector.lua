8ad34751ecf5bfd89ad92c585f9a90720f984c06
Alex Zasorin <zasorin.alek@gmail.com>
2025-12-24 23:48:14 -0800

    chore(nvim): remove venv-selector branch

--- dot_config/nvim/lua/plugins/venv-selector.lua
+++ dot_config/nvim/lua/plugins/venv-selector.lua
@@ -7,7 +7,6 @@ return {
     { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
   },
   ft = 'python',
-  branch = 'regexp', -- This is the regexp branch, use this for the new version
   keys = {
     { '<Leader>vf', '<cmd>VenvSelect<cr>' },
   },
