07c2090fe65e63a6cb0e777775d8f5be2b1e0d19
Alex Zasorin <zasorin.alek@gmail.com>
2025-12-31 22:09:20 -0800

    refactor(nixos): smb share mount to use sops credentials

--- dot_config/nixos/configurations/desktop.nix
+++ dot_config/nixos/configurations/desktop.nix
@@ -8,10 +8,16 @@
   fileSystems."/mnt/share" = {
     device = "//192.168.1.160/smb_alex";
     fsType = "cifs";
-    options = let
-      # FIXME: Refactor to use proper secret management
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "credentials=/etc/nixos/smb-secrets"
+      "uid=1000"
+      "gid=100"
+    ];
   };
 
   programs.steam.enable = true;
